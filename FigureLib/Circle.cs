974608442a03f0b5a750ffb50f20fb6f7c2716d7
Дмитрий Мазин <mitya.mazin@mail.ru>
2023-08-15 14:54:28 +0300

    Added a new interface "IShape" with method "GetArea", that allows to add new figures.

--- FigureLib/Circle.cs
+++ FigureLib/Circle.cs
@@ -6,17 +6,16 @@ using System.Threading.Tasks;
 
 namespace FigureLib
 {
-    public class Circle : Figure
+    public class Circle : IShape
     {
         public double Radius { get; set; }
         public Circle(double radius)
         {
             Radius = radius;
         }
-        public override double GetArea()
+        public double GetArea()
         {
-            Area = Math.PI * Math.Pow(Radius, 2);
-            return Area;
+            return Math.PI * Math.Pow(Radius, 2);
         }
     }
 }
--- FigureLib/Triangle.cs
+++ FigureLib/Triangle.cs
@@ -2,7 +2,7 @@
 
 namespace FigureLib
 {
-    public class Triangle : Figure
+    public class Triangle : IShape
     {
         public double SideA { get; set; }
         public double SideB { get; set; }
@@ -13,15 +13,21 @@ namespace FigureLib
             SideB = sideB;
             SideC = sideC;
 
-            if (Math.Pow(sideA, 2) + Math.Pow(sideB, 2) == Math.Pow(sideC, 2))
-                Console.WriteLine("Triangle is rectangular");
+            //if (Math.Pow(sideA, 2) + Math.Pow(sideB, 2) == Math.Pow(sideC, 2))
+            //    Console.WriteLine("Triangle is rectangular");
         }
-        public override double GetArea()
+        public double GetArea()
         {
             // The area of any triangle on three sides can be calculated using the Heron formula
             double halfPerimeter = (SideA + SideB + SideC) / 2.0;
-            Area = Math.Sqrt(halfPerimeter * (halfPerimeter - SideA) * (halfPerimeter - SideB) * (halfPerimeter - SideC));
-            return Area;
+            return Math.Sqrt(halfPerimeter * (halfPerimeter - SideA) * (halfPerimeter - SideB) * (halfPerimeter - SideC));
+        }
+
+        public bool IsRightTriangle()
+        {
+            return Math.Pow(SideA, 2) + Math.Pow(SideB, 2) == Math.Pow(SideC, 2) ||
+                   Math.Pow(SideB, 2) + Math.Pow(SideC, 2) == Math.Pow(SideA, 2) ||
+                   Math.Pow(SideC, 2) + Math.Pow(SideA, 2) == Math.Pow(SideB, 2);
         }
     }
 }
